b2eaa86bdcd3c20d516ade23ba8b57af7396c0d4
CN0073 <cn0073@CN0073.lan>
2019-06-07 17:25:56 +0100

    commit with css, js and html complete

--- app.js
+++ app.js
@@ -1,34 +1,41 @@
 
-let boom = new Audio("sounds/boom.wav");
-let clap = new Audio("sounds/clap.wav");
-let hihat = new Audio("sounds/hihat.wav");
-let kick = new Audio("sounds/kick.wav");
-let openhat = new Audio("sounds/openhat.wav");
-let ride = new Audio("sounds/ride.wav");
-let snare = new Audio("sounds/snare.wav");
-let tink = new Audio("sounds/tink.wav");
-let tom = new Audio("sounds/tom.wav");
+let C1 = new Audio("sounds/pianoC1.wav");
+let clap = new Audio("sounds/pianoC2.wav");
+let hihat = new Audio("sounds/pianoC3.wav");
+let kick = new Audio("sounds/pianoC7.wav");
+let openhat = new Audio("sounds/pianoFsharp0.wav");
+let ride = new Audio("sounds/pianoFsharp1.wav");
+let snare = new Audio("sounds/pianoFsharp2.wav");
+let tink = new Audio("sounds/pianoFsharp3.wav");
+let tom = new Audio("sounds/pianoFsharp4.wav");
+let item = new Audio("sounds/pianoFsharp6.wav");
+
 const x = document.body
 
-x.addEventListener('keypress', function(event) {
-    if (event.code == "KeyQ") {
-      tom.play();
+x.addEventListener('keydown', function(event) {
+      if (event.code == "KeyQ") {
+      tom.load(),tom.play();
     } if (event.code == "KeyW") {
-        tink.play();
+      tink.load(),tink.play();
     } if (event.code == "KeyE") {
-        snare.play();
+        snare.load(),snare.play();
     } if (event.code == "KeyR") {
-          ride.play();
+        ride.load(), ride.play();
     } if (event.code == "KeyT") {
-        openhat.play();
+        openhat.load(),openhat.play();
     } if (event.code == "KeyY") {
-          kick.play();
+        kick.load(),kick.play();
+    } if (event.code == "KeyU") {
+        item.load(),item.play();
     } if (event.code == "KeyI") {
-        hihat.play();
+        hihat.load(),hihat.play();
     } if (event.code == "KeyO") {
-          clap.play();
+        clap.load(),clap.play();
     } if (event.code == "KeyP") {
-        boom.play();
+        item.load(),item.play();
       }
 
   })
+
+ 
+  
